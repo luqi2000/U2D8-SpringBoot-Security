3889cc52d027b77c208ff87b7b58a875a40f77de
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-06-08 15:22:02 +0100

    Initial commit

---
+++ src/main/java/luqmamohammad/U2D8SpringBootSecurity/U2D8SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package luqmamohammad.U2D8SpringBootSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2D8SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2D8SpringBootSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/luqmamohammad/U2D8SpringBootSecurity/U2D8SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package luqmamohammad.U2D8SpringBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2D8SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
